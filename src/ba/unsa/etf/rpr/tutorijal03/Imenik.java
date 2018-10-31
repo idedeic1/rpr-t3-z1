9c3d935e3d3c54f6765dcbc7eabeef49676d3a06
Ibrahim Dedeic <idedeic1@etf.unsa.ba>
2018-10-31 17:32:26 +0100

    Fiksni Broj, haman gotovo

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -2,18 +2,23 @@ package ba.unsa.etf.rpr.tutorijal03;
 
 import java.util.HashMap;
 import java.util.Set;
-
+import java.util.Iterator;
 
 
 public class Imenik {
     private HashMap<TelefonskiBroj, String> kontakti = new HashMap<>();
+    public void dajIme{
+        kontakti
+    }
 
     public void dodaj(String ime, TelefonskiBroj broj){
         kontakti.put(broj,ime);
     }
 
     public String dajBroj(String ime){
-
+        Set set = kontakti.entrySet();
+        Iterator iterator = set.iterator();
+        
         return kontakti.get(ime);
     }
 
@@ -22,7 +27,14 @@ public class Imenik {
         return (String)kontakti.get(broj);
     }
 
-    public String naSlovo(char s){}
+    public String naSlovo(char s){
+        String tmp;
+        Iterator it = kontakti.iterator();
+        for(int i=0; i<kontakti.size(); i++){
+            if(kontakti.get(i).charAt(0) == s)
+                System.out.println(i, ".", kontakti. ));
+        }
+    }
     public Set<String> izGrada(Grad g){
         Set<String> temp;
 
